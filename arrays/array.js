3a895c806363c952e492b3169d45ddc87961848e
Didier <didorg@gmail.com>
2021-08-03 23:13:26 -0400

    filter

--- arrays/array.js
+++ arrays/array.js
@@ -1,6 +1,13 @@
 let fruits = ["Apples", "bananas", "mangoes"];
 let citrus = ['oranges', 'mandarins', 'limes'];
-let person = [];
+const scores = [10, 6, 7, 8, 4, 9, 10, 8];
+const stocks = [
+    { symbol: "AAPL", name: "Apple", premium: true },
+    { symbol: "TSLA", name: "Tesla", premium: true },
+    { symbol: "NIO", name: "NIO Inc.", premium: false },
+    { symbol: "AMD", name: "AMD", premium: true },
+    { symbol: "DIDI", name: "DiDi Global Inc.", premium: false },
+  ];
 
 console.log(fruits[1]);
 console.log(fruits.length);
@@ -18,6 +25,12 @@ let last = fruits.pop();
 console.log(last);
 console.log(fruits);
 
-//
+// ForEach
 const logCitrus = (c, index)=>{console.log(`${index} - ${c}`)}
 citrus.forEach(logCitrus);
+
+// Filter
+const premiumStocks = stocks.filter( stock =>{
+    return !stock.premium;
+});
+console.log(premiumStocks);
