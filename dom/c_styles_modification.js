a40766d9f1c207eafa9734c3640e0f6501ee0c2e
Didier <didorg@gmail.com>
2021-08-05 10:10:38 -0400

    Update c_styles_modification.js
    DOM Manipulation

--- dom/c_styles_modification.js
+++ dom/c_styles_modification.js
@@ -1,10 +1,14 @@
-
 const mssg = document.querySelector("p.error");
+console.log(mssg.style); // All styles in camelCase
 
 // Override the style
-mssg.setAttribute("style", "margin: 50px");
+mssg.setAttribute("style", "margin: 5px 10px 15px 20px");
 
 // add style
-mssg.style.color="red";
+mssg.style.color = "red";
 // javascript style-style as to be (camelCase)
-mssg.style.fontSize = '30px';
+mssg.style.fontSize = "30px";
+
+const content = document.querySelector(".content");
+content.style.fontSize = "50px";
+
