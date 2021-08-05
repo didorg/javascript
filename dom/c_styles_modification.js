d208647951b219e423ace7bb37386d5519f4cd9c
Didier <didorg@gmail.com>
2021-08-05 11:25:36 -0400

    DOM Manipulation

--- dom/c_styles_modification.js
+++ dom/c_styles_modification.js
@@ -5,10 +5,26 @@ console.log(mssg.style); // All styles in camelCase
 mssg.setAttribute("style", "margin: 5px 10px 15px 20px");
 
 // add style
-mssg.style.color = "red";
-// javascript style-style as to be (camelCase)
+// javascript style (camelCase)
 mssg.style.fontSize = "30px";
 
 const content = document.querySelector(".content");
-content.style.fontSize = "50px";
+content.style.fontSize = "1.5em";
+content.style.margin = '20px 20px';
 
+/** add and remove classes *****************************************/
+const container = document.querySelector('.container');
+console.log(container.classList);
+// container.classList.add('error');
+// container.classList.remove('error');
+
+const pInContainer = document.querySelectorAll('div.container > p');
+pInContainer.forEach(p=>{
+    if(p.innerText.includes('error')){ //innerText don't work with hidden elements
+        p.classList.add('error');
+    }
+    if(p.textContent.includes('success')){ //textContent work with hidden elements also
+        p.classList.add('success');
+    }
+})
+/** add and remove classes *****************************************/
