f3db6582edf322b61181f75adabb897a05a7886f
Didier <didorg@gmail.com>
2021-11-08 17:53:05 -0500

    algos

---
+++ quokka/js/algos/reverse_string.js
@@ -0,0 +1,11 @@
+const reverseString = (str) => {
+  let strReversed = "";
+  let top = str.length - 1;
+
+  for (top; top >= 0; top--) {
+    strReversed += str[top];
+  }
+  return strReversed;
+};
+
+console.log(reverseString("fedcba"));
