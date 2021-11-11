a703f00cb03303a0b018a8def6411609a3c1ad6e
Didier <didorg@gmail.com>
2021-11-11 00:10:21 -0500

    JS Best Practices

---
+++ quokka/js/bpIfElse.js
@@ -0,0 +1,28 @@
+//** Avoid If/Else | Switch
+//! Avoid
+const getCityInformation = (city) => {
+  if (city.toLowerCase() === "london") {
+    return "Welcome to London!";
+  } else if (city.toLowerCase() === "paris") {
+    return "Welcome to Paris!";
+  } else if (city.toLowerCase() === "amsterdam") {
+    return "Welcome to Amsterdam!";
+  }
+  return "We're Sorry, No City Found!";
+};
+const londonCityInfo = getCityInformation("Amsterdam");
+
+console.log(londonCityInfo);
+
+// ** Better
+const getCityInfo = (city) => {
+    const cityInfo = {
+      "london": "Welcome to London!",
+      "paris": "Welcome to Paris!",
+      "amsterdam": "Welcome to Amsterdam!",
+    };
+    return cityInfo[city.toLowerCase()] ?? "We're Sorry, No City Found!";
+  }
+  const parisCityInfo = getCityInfo('paris');
+  
+  console.log(parisCityInfo);
