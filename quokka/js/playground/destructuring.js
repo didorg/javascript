b331a8e7a1502c333f41b4c7f9a67e27e311ba47
Didier <didorg@gmail.com>
2021-11-16 12:19:14 -0500

    Destructuring

--- quokka/js/playground/destructuring.js
+++ quokka/js/playground/destructuring.js
@@ -76,8 +76,92 @@ const user = {
 };
 
 const userId = ({ id }) => id;
-const whois = ({ displayName, fullName: { firstName: name }}) => `${displayName} is ${name}`;
+const whois = ({ displayName, fullName: { firstName: name } }) =>
+  `${displayName} is ${name}`;
 
 console.log(userId(user)); // 42
 console.log(whois(user)); // jdoe is John
 
+// ** Nested object and array destructuring
+const metadata = {
+  title: "Scratchpad",
+  translations: [
+    {
+      locale: "de",
+      localization_tags: [],
+      last_edit: "2014-04-14T08:43:37",
+      url: "/de/docs/Tools/Scratchpad",
+      title: "JavaScript-Umgebung",
+    },
+  ],
+  url: "/en-US/docs/Tools/Scratchpad",
+};
+
+let {
+  title: englishTitle, // rename
+  translations: [
+    {
+      title: localeTitle, // rename
+    },
+  ],
+} = metadata;
+
+englishTitle; // "Scratchpad"
+localeTitle; // "JavaScript-Umgebung"
+
+const people = [
+  {
+    name: "Mike Smith",
+    family: {
+      mother: "Jane Smith",
+      father: "Harry Smith",
+      sister: "Samantha Smith",
+    },
+    age: 35,
+  },
+  {
+    name: "Tom Jones",
+    family: {
+      mother: "Norah Jones",
+      father: "Richard Jones",
+      brother: "Howard Jones",
+    },
+    age: 25,
+  },
+];
+
+for (const {
+  name: n,
+  family: { father: f },
+} of people) {
+  console.log(`Name: ${n}, Father: ${f}`);
+}
+
+// ** Combined Array and Object Destructuring
+// Say you want the third element in the array props below, and then you want the name property in the object.
+const props = [
+  { id: 1, name: "Fizz" },
+  { id: 2, name: "Buzz" },
+  { id: 3, name: "FizzBuzz" },
+];
+
+const [, , { name }] = props;
+
+name; // "FizzBuzz"
+
+// ** Return multiple values
+// Although multiple return values aren’t baked into the language proper, 
+// they don’t need to be because you can return an array and destructure the result:
+function returnMultipleValuesWithArr() {
+  return [1, 2];
+}
+var [fo, bas] = returnMultipleValuesWithArr();
+bas; // 2
+function returnMultipleValuesWithObj() {
+  return {
+    fobo: 1,
+    bara: 2
+  };
+}
+var { fobo, bara } = returnMultipleValuesWithObj();
+fobo; // 1
